3e28986a65028ba39fd2eab324ab1430a4aba25a
@ranveer-2301 <ranveerkrpoddar@gmail.com>
2026-07-02 07:47:07 +0530

    added cart

---
+++ src/main/java/com/app/ecom/repository/CartItemRepository.java
@@ -0,0 +1,19 @@
+package com.app.ecom.repository;
+
+import com.app.ecom.models.CartItem;
+import com.app.ecom.models.Product;
+import com.app.ecom.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    CartItem findByUserAndProduct(User user, Product product);
+
+    void deleteByUserAndProduct(User user, Product product);
+
+    List<CartItem> findByUser(User user);
+}
